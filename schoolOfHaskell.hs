bd40c1d1d2fda156edab42f0b441b37828a67782
Katie <57237449+Katie-N@users.noreply.github.com>
2023-11-27 12:22:03 -0600

    Finished code

--- schoolOfHaskell.hs
+++ schoolOfHaskell.hs
@@ -14,6 +14,7 @@
 -- This is necessary because I am using checks for the results of Either such as "isRight" and "fromRight"
 import Data.Either
 
+-- I modified the types from https://www.schoolofhaskell.com/user/adlew/calculator in order to match the types given in our instructions
 -- Define the custom types we will use
 -- We want to make an operatorRegister that contains a string to represent the operation, and the function itself to apply
 -- So the Register type should be a list of something. We call it Entry
@@ -23,15 +24,21 @@ type Entry = (String, Operator)
 type Operator = Double -> Double -> Double
 
 -- This is a function that was given from https://www.schoolofhaskell.com/user/adlew/calculator
--- I did not do change anything with it
+-- I did not change anything with it
 -- It works by using the modulo operation on two integers it is passed.
 -- Since we were working with Doubles, but mod needs integers, we had to round the Doubles.
--- The $ is a way to say that the function should be evaluated last. Normally in Haskell the function gets evaluated ASAP. But this is another way of writing fromIntegral (mod (round a) (round b))
+-- The $ is a way to say that the function should be evaluated last.
+-- Normally in Haskell the function gets evaluated ASAP. But this is another way of writing: fromIntegral (mod (round a) (round b))
 modulu :: Double -> Double -> Double
 -- fromIntegral is used to convert to a Double
+-- round is used to convert to an int
+-- mod is the built in modulo operator in Haskell
 modulu a b = fromIntegral $ mod (round a) (round b)
 
--- The lower the character in the register, the higher precedence it has
+isInf val = if (show val == "Infinity") then True else False
+
+-- The farther down in the register the function is, the higher precedence it has.
+-- This is done using span and recursively calling eval within the eval function. See there for more details
 -- operatorRegister is a list of tuples that take the form (String, on)
 -- Register is a special type defined above.
 -- I added the exponent operator but I got the rest from https://www.schoolofhaskell.com/user/adlew/calculator
@@ -46,9 +53,11 @@ operatorRegister = [
                 ("**", (**)) -- I added the exponent operator and it was as simple as appending it to this list of operators
             ]
 
+-- I modified this function from this StackOverflow answer about how to get a substring https://stackoverflow.com/a/13962931
 -- countParentheses takes in a string and returns a list of tuples of the form ('characterMatched', occurrencesInt)
--- It is searching for open and close parentheses so we can use this function to check if there is an invalid amount of parentheses. I modified it from this StackOverflow answer about how to get a substring https://stackoverflow.com/a/13962931
--- It works by returning a list of tuples. Anytime the specified characters are found in the string, their count is incremented. When the string has finished, the character that was found and the number of occurrences is added to the list as a tuple.
+-- It is searching for open and close parentheses so we can use this function to check if there is an invalid amount of parentheses.
+-- It works by returning a list of tuples. Anytime the specified characters are found in the string, their count is incremented.
+-- When the string has finished, the character that was found and the number of occurrences is added to the list as a tuple.
 countParentheses string = [ (x,c) | x<-['(', ')'], let c = (length.filter (==x)) string, c>0 ]
 
 altParenth str =
@@ -85,10 +94,10 @@ handleParenth stringExpression =
       -- This next expression (innerExpression) took a while to come up with. I had to build it up piece by piece:
         -- Basically I am taking the string and I need to pull out a piece of the middle.
         -- So I need to drop a certain number of characters from the beginning of the string and the end of the string
-        -- To drop from the front, I can use the drop function. I pass it the number of characters before the first opening parentheses. I also add 1 because I want to skip the '('
+        -- To drop from the front, I can use the built in drop function.
+        -- I pass it the number of characters before the first opening parentheses. I also add 1 because I want to skip the '('
         -- To drop from the end, I will take less characters from the string then it is long.
-        -- I figured out how many characters to take by subtracting the total string length, the length of the string after the last closing parentheses,
-        -- and I subtracted a 1 because I wanted to skip the ')'.
+        -- I figured out how many characters to take by subtracting the total string length, the length of the string after the last closing parentheses, and 1 because I wanted to skip the ')'.
       innerExpression = drop (length beforeParentheses + 1) (take ((length stringExpression) - (length afterParentheses) - 1) stringExpression)
 
       -- In case the result of the calculation does not return a valid Double
@@ -98,8 +107,7 @@ handleParenth stringExpression =
   in
     -- return the original string but replace the parentheses with the value
     if isRight innerValueUnsafe -- must be a Double
-      then
-        Right (beforeParentheses ++ show (fromRight 0 innerValueUnsafe) ++ afterParentheses)
+      then Right (beforeParentheses ++ show (fromRight 0 innerValueUnsafe) ++ afterParentheses)
       else
         Left (fromLeft "Error: Unexpected error" innerValueUnsafe) -- this must be an error value so we will just pass it back
 
@@ -122,7 +130,7 @@ calculate stringExpression = if length (countParentheses stringExpression) > 0
         -- recursively call calculate with the new string expression containing one less pair of parentheses
         -- Return it as the "Right" value of this function by extracting the "Right" value from the function call. fromRight takes a default value and the value to pass if it is of the "Right" type.
         -- The default value (0) shouldn't ever be passed because of our error checking up to this point. But it is required just in case.
-        in -- Right (fromRight 0 (calculate stringWithoutInnerParenth))
+        in
           if isRight unsafeStringWithoutInnerParenth
             then calculate (fromRight "0" unsafeStringWithoutInnerParenth)
             -- unfortunately, even though this error is the same type and we know it must be an error, Haskell will not compile if we just return the unsafeStringWithoutInnerParenth
@@ -157,7 +165,7 @@ calculate stringExpression = if length (countParentheses stringExpression) > 0
 -- It allows us to have error checking more easily and handle errors more gracefully than throwing exceptions
 eval :: Register -> [String] -> Either String Double
 eval [] _ = Left "Error: Missing operator" -- If eval is called with an empty operator register then there is no operator found.
-eval _ [] = Left "Error: Operators Without Operands" -- If a operator don't have anything to operate on. (Missing argument)
+eval _ [] = Left "Error: Operators Without Operands" -- If an operator doesn't have anything to operate on. (Missing argument)
 eval _ [singleVal] =
   if elem singleVal ["+", "-", "*", "/", "%", "(", ")"]
     -- singleVal must be an operator (and has no numbers to operate on!) so we return an error
@@ -187,7 +195,12 @@ eval ((operator, function):rest) unparsed = -- This is where most of the heavy l
           -- Because we recursively find the values of arg1 and arg2, and span matches elements UNTIL the condition is met, we are able to follow the order of operations
           -- By this point, arg1 and arg2 are doubles so we can apply the matched function to them
           -- Perform the operation on the first and second argument and return it as a Right value
-          Right (function arg1 arg2) -- function is the parameter name we defined in this pattern of eval
+
+          -- I created this part to check for division by 0
+          -- If division by 0 occurs it will be returned as "Infinity." But we only want real answers so we will return an error if this happens
+          -- Otherwise we will return the result of applying the function to the arguments
+          if isInf (function arg1 arg2) then Left "Error: Invalid Operator Usage" else Right (function arg1 arg2)
+          -- Note that "function" is a parameter name we defined in this pattern of eval
 
 -- I created this function
 -- It is what the user will interface with on the console
@@ -208,6 +221,7 @@ parse stringExpression =
     -- At least one character in the string is not allowed. We return a string not a Left value (because this is getting returned to the user, not another function).
     else "Error: Invalid characters"
 
+-- I created this function
 -- Check if every character in the string is in the valid characters list
 -- all will return true when every item in a list meets a condition.
   -- Our list is the passed string.
